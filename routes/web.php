db6f1553906c15f486af92b943528ebdff54744f
iqbalproyek <iqbalproyek@gmail.com>
2022-01-18 12:06:24 +0700

    membuat file login dan dashboard, dan membuat component template web

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
 
